2f61365d0be4b534b2ce41aa81b4cbcf2214dd56
AndreaPrz28 <andreaprz258@gmail.com>
2025-06-19 16:29:28 -0600

    Logica de movimiento total. Acciones de salida y reinicio

--- Laberinto.asm
+++ Laberinto.asm
@@ -1,15 +1,13 @@
 section .text
     global mover_personaje
 
-;rcx = puntero al arreglo(arr)
-;rdx=longitud del arreglo
-
 mover_personaje:
     ; rcx   ; En este registro vamos a guardar el puntero a la matriz
     ; dx   ; En este guardaremos los renglones de la matriz
     ; r8   ; En este guardaremos las columnas de la matriz
     ; r9   ; En este guardamos la tecla ingresada
 
+    ; Estos son los elementos de la pila que vamos a usar
     push rbx
     push rsi
     push rdi
@@ -20,57 +18,114 @@ mover_personaje:
     imul rdx, r8   ; multiplicamos filas X columnas, para tener el total de elementos
     mov rbp, r9   ; es el caracter que nos dicta el movimiento
 
-    .buscar:
+    ; Aqui vamos a buscar donde esta la letra 'P'    
+    .buscar_P:
         cmp rbx, rdx   ; Hacemos una validacion si indice > total de elementos
-        jge .fin       ; salta si es mas grande o igual
+        jge .fin       ; Salta si es mas grande
 
-        mov al, [rsi + rbx]   ; Colocamos en ax el valor actual
-        cmp al, 'P'
-        je .avanzar
+        mov al, [rsi + rbx]   ; Colocamos en al el valor de la posicion actual de la "matriz"
+        cmp al, 'P'    ; Comparamos matriz[i][j] == 'P'
+        je .avanzar     ; Si es igual, saltamos a una subrutina donde realizaremos
+                        ; el movimiento que la tecla nos indique
 
-        inc rbx
-    jmp .buscar
+        inc rbx    ; Si no es igual se aumenta el contador hasta encontrala
+    jmp .buscar_P
 
+    ; Aqui realizaremos el movimiento de la 'P'
     .avanzar:
-        cmp rbp, 'd'
+        cmp rbp, 'd'   ; Si tecla == d el movieminto es a la derecha
         je .derecha
         
-        cmp rbp, 'a'
+        cmp rbp, 'a'   ; Si tecla == a el movieminto es a la derecha
         je .izquierda
 
-        cmp rbp, 'w'
+        cmp rbp, 'w'   ; Si tecla == w el movieminto es a la derecha
         je .arriba
 
-        cmp rbp, 's'
+        cmp rbp, 's'   ; Si tecla == s el movieminto es a la derecha
         je .abajo
+    jmp .fin     
 
-        jmp .fin
-
+    jmp .fin
+        
+        ; Funcion para moverse a la derecha
         .derecha:
-            mov al, '.'
-            mov [rsi + rbx], al
-            mov al, 'P'
-            inc rbx
+            inc rbx   ; Incrementamos en 1 para ver que hay en la posicion derecha
+            mov al, [rsi + rbx]   ; Asignamos a al la posicion siguiente
+            cmp al, '#'   ; Y si en dicha posicion ha un '#' no se puede avanzar
+            je .fin   
+
+                      ; ~~~ En caso de que el jump no salte ~~~
+
+            dec rbx   ; Decrementamos para volver a la posicion inicial
+                      ; pero sabiendo que si podremos avanzar al siguiente espacio
+
+            mov al, '.'   ; En al colocamos un '.' para poder asignarlo despues al espacio correspondiente de la "matriz"
+            mov [rsi + rbx], al   ; A la posicion actual le asignamos lo que tiene al
+            inc rbx   ; Incrementamos en 1 la posicion de la matriz para avanzar al siguiente  
+                      ; sigueinte espacio de la matriz 
+
+            mov al, 'P'   ; En 
             mov [rsi + (rbx)], al
             jmp .fin
 
+        ; Funcion para moverse a la izquierda
         .izquierda:
+            dec rbx   ; Decrementamos en 1 para ver que hay en la posicion a la izquierda
+            mov al, [rsi + rbx]
+            cmp al, 35
+            je .fin
+
+            inc rbx
+
             mov al, '.'
             mov [rsi + rbx], al
-            mov al, 'P'
             dec rbx
+
+            mov al, 'P'
             mov [rsi + (rbx)], al
             jmp .fin
 
+        ; Funcion para moverse arriba
         .arriba:
+            sub rbx, 12
+            mov al, [rsi + rbx]
+            cmp al, '#'
+            je .fin
 
+            add rbx,12
+
+            mov al, '.'
+            mov [rsi + rbx], al
+            sub rbx, 12
+
+            mov al, 'P'
+            mov [rsi + rbx], al
+            jmp .fin
+
+        ; Funcion para moverse abajo
         .abajo:
+            add rbx, 12
+            mov al, [rsi + rbx]
+            cmp al, '#'
+            je .fin
 
+            sub rbx, 12
+
+            mov al, '.'
+            mov [rsi + rbx], al
+            add rbx, 12
+
+            mov al, 'P'
+            mov [rsi + rbx], al
+            jmp .fin
 
     .fin:
+        ; Regresamos los elementos de la pila que tomamos
         pop rbp
         pop rdi
         pop rsi
         pop rbx
-
-        ret                    
+        
+        ; Retornamos la matriz con el movimiento del personaje ya realizado
+        ret                
--- Main.c
+++ Main.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>   // Para los printf() y los scanf()
+#include <stdlib.h>   // Para el system("cls")
+#include <Windows.h>   // Para los Sleep
 
 #define REN 12
 #define COL 12
@@ -14,14 +15,29 @@ int main(){
     int op = 0;
 
     do{
-        op = menu();
-        op = display();
+        // El if es para que no entre al menu si en "display" seleccionamos 
+        // la opcion de reiniciar el juego
+        if (op != 1){
+            op = menu();
+        }
 
-    }while(op!=5);
+        // Este if es para que no entremos al displey en caso de seleccionar
+        // la opcion de salir en el "menu"
+        if (op != 2){
+            op = display();
+        }
+
+    }while(op!=2);
+
+    printf("\n\n\n\n    ~~~~~ HASTA PRONTO ~~~~~");
+    Sleep(1000);
+    system("cls");
+
+    return 0;
 }
 
 int menu(){
-    int resp = 0;
+    int resp = 0; 
 
     printf("\n************************************\n\n");
     printf("~~~ JUEGO DEL LABERINTO ~~~\n");
@@ -32,6 +48,9 @@ int menu(){
     scanf("%d", &resp);
     printf("\n************************************\n\n");
 
+    Sleep(1500);
+    system("cls");
+
     return resp;
 }
 
@@ -39,6 +58,8 @@ int display(){
 
     int opcion=0;
     char tecla;
+
+    // Aqui creamos nuestra matriz y laberinto
 
     char mapa[REN][COL] = {
                         {'#','#','#','#','#','#','#','#','#','#','#','#'},
@@ -51,30 +72,51 @@ int display(){
                         {'#','.','.','.','.','.','.','#','#','#','#','#'},
                         {'#','.','#','#','#','#','.','#','.','.','.','#'},
                         {'#','.','#','.','#','#','.','#','.','#','.','#'},
-                        {'#','.','.','.','.','#','.','.','.','#','X','#'},
+                        {'#','.','.','.','.','#','.','.','.','#','X','#'},  // La x esta en la posicion mapa[10][10]
                         {'#','#','#','#','#','#','#','#','#','#','#','#'}
     };
 
+    // Adentro de este do-while es donde vamos a estar haciendo los moviminetos para el juego
     do{
+    
+        // En estos for imprimimos la matriz e instrucciones que se estaran viendo en pantalla
         for (int i=0; i<REN; i++){
             for (int j=0; j<COL; j++){
                 printf(" %c", mapa[i][j]);
             }
             printf("\n");
         }
+
         printf("\n||| Usa W/A/S/D para moverte |||");
-        printf("\n     Presiona E para salir\n\n");
+        printf("\n    Presiona r para reiniciar");
+        printf("\n      Presiona e para salir\n\n");
         
+        // Capturamos la tecla que nos indicara el movimiento
         while ((tecla = getchar()) == '\n');
+
+        // Si es r nos regresa al main, y el main nos trae de vuelta al display
+        if (tecla == 'r'){
+            system("cls");
+            Sleep(800);
+            return 1;
+        }
+
+        // Si es e, nos regresa al main y saldremos del programa
+        if (tecla == 'e'){
+            system("cls");
+            Sleep(800);
+            return 2;
+        }
         
+        // Aqui es donde mandamos nuestra informacion a lenguaje ensamblador.
+        // Mandamos la matriz, renglones, columnas y la tecla seleccionada
         mover_personaje((char *)mapa,REN,COL,tecla);
-        
-        printf("\n     Presiona E para salir\n\n");
-        //printf(" ~~~ Reiniciar (1)");
-        //printf(" ~~~ Salir (2)");
-        opcion++;
+
+        // Aqui borramos la pantalla para que el movimiento de la 'P' 
+        // se pueda apreciar mejor y se vea mas fluido
+        system("cls");
     
-    }while(opcion != 5);
+    }while(opcion != 2);
 
     return opcion;
 }
